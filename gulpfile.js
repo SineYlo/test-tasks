572f33b283aa3b90eb09e24246edd17e44f5d575
SineYlo <vavshhik01@mail.ru>
2024-12-01 13:27:37 +1000

    feat: gulp build is ready to perform tasks

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const del = require('del');
+const rename = require('gulp-rename');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const connect = require('gulp-connect');
+
+function deleteDestinationDirectory() {
+  return new Promise((resolve) => {
+    del.sync('./dist/');
+
+    resolve();
+  });
+}
+
+function transferFontFiles() {
+  return new Promise((resolve) => {
+    src('./src/fonts/**/*.woff2')
+      .pipe(rename({
+        dirname: './'
+      }))
+      .pipe(dest('./dist/fonts/'));
+
+    resolve();
+  });
+}
+
+function transferJsFiles() {
+  return new Promise((resolve) => {
+    src('./src/js/pages/*.js')
+      .pipe(dest('./dist/js/'))
+      .pipe(connect.reload());
+
+    resolve();
+  });
+}
+
+function transpilePug() {
+  return new Promise((resolve) => {
+    src('./src/pug/pages/*.pug')
+      .pipe(pug())
+      .pipe(dest('./dist/'))
+      .pipe(connect.reload());
+
+    resolve();
+  });
+}
+
+function transpileScss() {
+  return new Promise((resolve) => {
+    src('./src/scss/pages/*.scss')
+      .pipe(sass())
+      .pipe(dest('./dist/css/'))
+      .pipe(connect.reload());
+
+    resolve();
+  });
+}
+
+function createServer() {
+  return new Promise((resolve) => {
+    connect.server({
+      root: './dist/',
+      livereload: true
+    });
+
+    resolve();
+  })
+}
+
+watch('./src/pug/**/*.pug', transpilePug);
+watch('./src/scss/**/*.scss', transpileScss);
+watch('./src/js/**/*.js', transferJsFiles);
+
+exports.dev = series(
+  deleteDestinationDirectory,
+  parallel(
+    transferFontFiles,
+    transferJsFiles
+  ),
+  parallel(
+    transpilePug,
+    transpileScss
+  ),
+  createServer
+);
